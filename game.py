db936ce1b8e3ff824b9460e03667239b4ca6fddb
peaceheis <navylsc4ever@gmail.com>
2021-11-18 21:25:22 -0600

    json done, groups are now dicts not custom objects

--- game.py
+++ game.py
@@ -1,30 +1,37 @@
 import json
 import random
 
-from constants import *
-import group
+import PySimpleGUI as sg
+from property import Property
+from specials import Railroad, Utility
 from player import Player
 
 
 class Game: 
     def __init__(self, players=[], turn=0, dice=[1, 1]):
+        if (self.get_save_if_possible()) is not None: 
+            self.should_load_save = True #i suspect this is unpythonic.        
+
         self.groups = self.load_groups()
         self.players = players
         self.num_players = len(players)
         self.turn = turn
         self.dice = dice
-        
-        self.get_players()
 
-    def load_tiles(self):
-        streets = json.load(open("tiles.json"))["properties"]["streets"]
+    def get_save_if_possible(self): 
+        pass
 
     def turn(self): 
+
         self.turn = self.turn + 1 % self.num_players
+
         player = self.players[self.turn]
+
         self.popup(f"{player}'s turn!")
+
         self.roll_dice_visually()
         self.dice = [random.randrange(1, 7) for i in range(2)]
+        
         player.position = player.position + self.dice % 36
 
 
@@ -43,24 +50,53 @@ class Game:
     def should_continue_game(self): 
         pass
 
-
     def save_game(self): 
         pass
 
     def load_save(self): 
         pass
 
-    def load_groups() -> list: 
-        groups: dict = json.load(open("groups.json"))["groups"]
+    def load_groups(self): 
+        return json.load(open("groups.json"))["groups"]
 
-        initalized_groups = []
-        for val in groups.values() : 
-            prop = group.Group(val["name"], val["color"], val["house cost"])
-            initalized_groups.append(prop)
+    def load_tiles(self):
+        props = json.load(open("tiles.json"))["properties"]
+        
+        #load streets
+        streets: dict = props["streets"]
+       
+        initialized_streets = []
+        for key in streets.keys():
+            val = streets[key]
+            grp: dict = self.groups[val["group"]]
+            prop = Property(val["name"], int(key), val["cost"], grp["house cost"], grp)
+            self.groups[val["group"]]["properties"].append(prop)
+            initialized_streets.append(prop)
 
-        return initalized_groups 
+        #load railroads
+        rails: dict = props["railroads"]
+        config: dict = props["railroad config"]
+
+        initialized_rails = []
+        for key in rails.keys(): 
+            val = rails[key]
+            prop = Railroad(val, int(key), config["cost"], config["rent"])
+            initialized_rails.append(prop)
+
+        #load utilities
+        utils: dict = props["utilities"]
+        config: dict = props["utility config"]
+
+        initialized_utils = []
+        for key in utils.keys(): 
+            val = utils[key]
+            prop = Utility(val, int(key), config["cost"], config["rent"])
+            initialized_utils.append(prop)
+
+        return initialized_streets, initialized_rails, initialized_utils
+
+            
 
 
-winCount, loseCount = 0
-
-Game()
+g = Game()
+g.load_tiles()
